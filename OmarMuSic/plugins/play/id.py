94433e0d6c65c47277013535db26ea68b3e1a686
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-13 21:37:10 +0200

    Update id.py

--- OmarMuSic/plugins/play/id.py
+++ OmarMuSic/plugins/play/id.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from YukkiMusic import app
+from OmarMuSic import app
 import random
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
@@ -59,65 +59,3 @@ async def iddd(client, message):
             ]
         ),
     )
-
-
-
-iddof = []
-@app.on_message(
-    command(["قفل جمالي","تعطيل جمالي"])
-    & filters.group
-  
-)
-async def lllock(client, message):
-   get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in ["creator", "administrator"]:
-      if message.chat.id in iddof:
-        return await message.reply_text("جمالي معطل من قبل✅")
-      iddof.append(message.chat.id)
-      return await message.reply_text(" تم تعطيل جمالي بنجاح✅🔒")
-   else:
-      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
-
-@app.on_message(
-    command(["فتح جمالي","تفعيل جمالي"])
-    & filters.group
-  
-)
-async def idljjopen(client, message):
-   get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in ["creator", "administrator"]:
-      if not message.chat.id in iddof:
-        return await message.reply_text("جمالي مفعل من قبل✅")
-      iddof.remove(message.chat.id)
-      return await message.reply_text("تم فتح جمالي بنجاح ✅🔓")
-   else:
-      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
-
-
-
-
-@app.on_message(
-    command(["جمالي"])
-    & filters.group
-  
-)
-async def idjjdd(client, message):
-    if message.chat.id in iddof:
-      return
-    usr = await client.get_chat(message.from_user.id)
-    name = usr.first_name
-    i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
-    ik = random.choice(i)
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"نسبه جمالك يا مز انت \n│ \n└ʙʏ: {ik} %😂❤️", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        ),
-    )
-       
-
