bc7a8a55bb018e6d2379f6a6b63e70a76a62bdb2
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-14 01:15:57 +0200

    Update المالك.py

--- OmarMuSic/plugins/play/المالك.py
+++ OmarMuSic/plugins/play/المالك.py
@@ -8,8 +8,8 @@ from pyrogram import filters
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from OmarMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from OmarMusic import app
+from OmarMuSic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from OmarMuSic import app
 from telegraph import upload_file
 from asyncio import gather
 from pyrogram.errors import FloodWait
