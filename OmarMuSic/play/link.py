096ca46dc03442fee2401019710d1cde9ed1a2eb
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-14 02:20:45 +0200

    Add files via upload

---
+++ OmarMuSic/play/link.py
@@ -0,0 +1,23 @@
+#𝙲𝙷.𝚂𝙾𝚄𝚁𝙲𝙴 : @WA_ADRENALEN
+#𝙳𝙴𝚅 𝙰𝙳𝚁𝙴𝙽𝙰𝙻𝙴𝙽 : @DEV_ADRENALEN
+#𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @BAR_ADRENALEN
+#Omar AdRenalen تم التعديل بواسطة 🎸 ⋅
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from OmarMuSic import app
+
+@app.on_message(filters.command(["الرابط","/link"], "") & filters.group & ~filters.private)
+async def invitelink(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        return await message.reply_text("قم برفعي مسؤول في المجموعة أولا ؟")
+    await message.reply_text(f"**تم إنشاء رابط الدعوة بنجاح :**\n {invitelink}")
+    
+#𝙲𝙷.𝚂𝙾𝚄𝚁𝙲𝙴 : @WA_ADRENALEN
+#𝙳𝙴𝚅 𝙰𝙳𝚁𝙴𝙽𝙰𝙻𝙴𝙽 : @DEV_ADRENALEN
+#𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @BAR_ADRENALEN
+#Omar AdRenalen تم التعديل بواسطة 🎸 ⋅
