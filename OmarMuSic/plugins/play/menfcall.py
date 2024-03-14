135f2d4889d39789eaba96613c52313ae754cab7
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-14 13:51:27 +0200

    Update menfcall.py

--- OmarMuSic/plugins/play/menfcall.py
+++ OmarMuSic/plugins/play/menfcall.py
@@ -3,31 +3,30 @@
 #𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @BAR_ADRENALEN
 #Omar AdRenalen تم التعديل بواسطة 🎸 ⋅
 
-from pyrogram import filters, Client
-from OmarMuSic import app
+ from pyrogram import filters, Client
+from ZeMusic import app
 import asyncio
 from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from OmarMuSic.core.call import Omar
-from OmarMuSic.utils.database import *
+from ZeMusic.core.call import Mody
+from ZeMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
-                     
 @app.on_message(filters.regex("^مين في الكول$"))
 async def strcall(client, message):
-    assistant = await group_assistant(Yukki,message.chat.id)
+    assistant = await group_assistant(Mody,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/vega.mp3"), stream_type=StreamType().pulse_stream)
-        text="🙃🔔 الارانب  المتواجدين في الكول :\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("./ZeMusic/assets/call.mp3"), stream_type=StreamType().pulse_stream)
+        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث 🗣"
+                mut="يتحدث 🗣 "
             else:
-                mut="ساكت 🔕"
+                mut="ساكت 🔕 "
             user = await client.get_users(participant.user_id)
             k +=1
             text +=f"{k}➤{user.mention}➤{mut}\n"
@@ -36,11 +35,11 @@ async def strcall(client, message):
         await asyncio.sleep(7)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"ارنب الكول مش مفتوح اصلااا\n🤔")
+        await message.reply(f"عمووووو الكول مش مفتوح اصلااا\n❌")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n🤔")
+        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n❌")
     except AlreadyJoinedError:
-        text="🙃🔔 الارانب  المتواجدين في الكول:\n\n"
+        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
@@ -48,14 +47,17 @@ async def strcall(client, message):
             if info.muted == False:
                 mut="يتحدث 🗣"
             else:
-                mut="ساكت 🔕"
+                mut="ساكت 🔕 "
             user = await client.get_users(participant.user_id)
             k +=1
             text +=f"{k}➤{user.mention}➤{mut}\n"
         text += f"\nعددهم : {len(participants)}\n✔️"    
         await message.reply(f"{text}")
-@app.on_message(filters.video_chat_ended)
+@app.on_message(filters.video_chat_started)
 async def brah(client, message):
+       await message.reply(" بدأت المحادثة الصوتية 👤")
+@app.on_message(filters.video_chat_ended)
+async def brah2(client, message):
     da = message.video_chat_ended.duration
     ma = divmod(da, 60)
     ho = divmod(ma[0], 60)
@@ -91,11 +93,11 @@ async def brah(client, message):
             await message.reply(f" تم إنهاء مكالمة الفيديو مدتها {day[0]} يوم")
 @app.on_message(filters.video_chat_members_invited)
 async def fuckoff(client, message):
-           text = f"• قام ← {message.from_user.mention}"
+           text = f"• قــــام ← {message.from_user.mention}"
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-               text += f"\n• بدعوة ←[{user.first_name}](tg://user?id={user.id})"
+               text += f"\n• بــدعـــوة ←[{user.first_name}](tg://user?id={user.id})"
                x += 1
              except Exception:
                pass
@@ -103,6 +105,7 @@ async def fuckoff(client, message):
              await message.reply(f"{text}")
            except:
              pass  
+
              
              
 #𝙲𝙷.𝚂𝙾𝚄𝚁𝙲𝙴 : @WA_ADRENALEN
