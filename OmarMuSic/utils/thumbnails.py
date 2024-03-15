8a5ecc375d02ff57d261ac62f4bb354cdc4f3105
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-15 06:05:20 +0200

    Update thumbnails.py

--- OmarMuSic/utils/thumbnails.py
+++ OmarMuSic/utils/thumbnails.py
@@ -30,8 +30,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from youtubesearchpython.__future__ import VideosSearch
 from config import YOUTUBE_IMG_URL
-from OmarMuSic import BOT_ID, LOGGER, app
-
+from OmarMuSic import LOGGER
 
 
 def changeImageSize(maxWidth, maxHeight, image):
