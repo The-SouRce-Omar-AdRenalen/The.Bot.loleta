0ad3870eed2f1c98ea561ca361a75ab3524934c9
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-26 02:52:09 +0200

    Update اونر.py

--- OmarMuSic/plugins/play/اونر.py
+++ OmarMuSic/plugins/play/اونر.py
@@ -177,14 +177,17 @@ async def iddd(client, message):
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"""🤡 ¦𝙽𝙰𝙼𝙴 :{message.from_user.mention}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{message.from_user.username}\n🎃 ¦𝙸𝙳 :{message.from_user.id}\n💌 ¦𝙱𝙸𝙾 :{usr.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :{message.chat.id}""", 
     reply_markup=InlineKeyboardMarkup(
+        [
             [
-                [
+                    InlineKeyboardButton(f"{idd} 🤍", callback_data=f"heart{usr.id}"),
+                ],[
                     InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
+                        name, url=f"https://t.me/{message.from_user.username}"),
             ]
-        ),
-    )    
+        ]
+         ),
+     )
+
 ##############################################################
 ##############################################################
 ##############################################################
